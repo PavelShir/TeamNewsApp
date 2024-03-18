afe83df60d3939cfc7194ed0c589efded15ffb9d
Pavel Shirokiy <pshirokiy@gmail.com>
2024-03-19 02:11:10 +0300

    do stract Categories

---
+++ TeamNewsApp/Models/Categories.swift
@@ -0,0 +1,32 @@
+//
+//  Categories.swift
+//  TeamNewsApp
+//
+//  Created by Павел Широкий on 19.03.2024.
+//
+
+import UIKit
+
+struct Category {
+    let image: String
+    let name: String
+}
+
+struct Categories {
+    
+    let categories = [
+    Category(image: "🏈", name: "Sport"),
+    Category(image: "⚖️", name: "Politics"),
+    Category(image: "🌞", name: "Life"),
+    Category(image: "🎮", name: "Gaming"),
+    Category(image: "🐻", name: "Animals"),
+    Category(image: "🌴", name: "Nature"),
+    Category(image: "🍔", name: "Food"),
+    Category(image: "🎨", name: "Art"),
+    Category(image: "📜", name: "History"),
+    Category(image: "👗", name: "Fashion"),
+    Category(image: "😷", name: "Covid-19"),
+    Category(image: "⚔️", name: "Middle East"),
+    ]
+    
+}
