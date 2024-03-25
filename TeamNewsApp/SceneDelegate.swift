2e50467ee9b814967fe971867d4c5000620f8ee8
Pavel Shirokiy <109968432+PavelShir@users.noreply.github.com>
2024-03-26 00:16:52 +0300

    Revert "Создал APIService, BrowseVC,NewsCollectionViewCell"

--- TeamNewsApp/SceneDelegate.swift
+++ TeamNewsApp/SceneDelegate.swift
@@ -15,13 +15,11 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         
-        let navigationController = UINavigationController(rootViewController: BrowseViewController())
-        navigationController.navigationBar.prefersLargeTitles = true
+        let window = UIWindow(windowScene: windowScene)
+        window.rootViewController = TabBarViewController()
+        window.makeKeyAndVisible()
         
-        window = UIWindow(windowScene: windowScene)
-        window?.rootViewController = TabBarViewController()
-        window?.makeKeyAndVisible()
-        
+        self.window = window
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
