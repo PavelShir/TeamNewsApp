8eca25b234737c0f853745717ce871daf2a1c39e
Maria Nesterova <ridebyhorse@gmail.com>
2024-03-26 17:00:30 +0300

    fix loginVC, add ability to choose photo on Profile
    fix loginVC, add transitions to TabBar, add ability to choose photo on Profile

--- TeamNewsApp/Model/UserStorageManager.swift
+++ TeamNewsApp/Model/UserStorageManager.swift
@@ -10,46 +10,76 @@ import UIKit
 final class UserStorageManager {
     
     static let shared = UserStorageManager()
+    private static var currentUser: User?
     private var userDefaults = UserDefaults.standard
-    private let key = "user"
+    private let key = "users"
 
-    func saveUserData(_ user: User) {
-        set(object: user, forKey: key)
+    func saveNewUser(_ user: User) {
+        UserStorageManager.currentUser = user
+        if var users = get(forKey: key) {
+            users.append(user)
+            set(object: users, forKey: key)
+        } else {
+            set(object: [user], forKey: key)
+        }
     }
     
     func updateUserName(name: String) {
-        guard var user = get(forKey: key) else { return }
-        user.name = name
-        set(object: user, forKey: key)
+        if var users = get(forKey: key) {
+            if var currentUser = UserStorageManager.currentUser {
+                let index = users.firstIndex(of: currentUser)
+                currentUser.name = name
+                UserStorageManager.currentUser = currentUser
+                if let index {
+                    users.remove(at: index)
+                    users.insert(currentUser, at: index)
+                    set(object: users, forKey: key)
+                }
+            }
+        }
     }
     
-    func updateUserPhoto(photo: UIImage) {
-        guard var user = get(forKey: key) else { return }
-        user.photo = ImageWrapper(image: photo)
-        set(object: user, forKey: key)
+    func updateUserPhoto(photoName: String) {
+        if var users = get(forKey: key) {
+            if var currentUser = UserStorageManager.currentUser {
+                let index = users.firstIndex(of: currentUser)
+                currentUser.photoName = photoName
+                UserStorageManager.currentUser = currentUser
+                if let index {
+                    users.remove(at: index)
+                    users.insert(currentUser, at: index)
+                    set(object: users, forKey: key)
+                }
+            }
+        }
     }
     
     func login(mail: String, password: String) -> Bool {
-        guard let user = get(forKey: key) else { return false }
-        if user.mail == mail && user.password == password {
-            return true
+        if var users = get(forKey: key) {
+            for user in users {
+                if user.mail == mail && user.password == password {
+                    UserStorageManager.currentUser = user
+                    return true
+                }
+            }
+            return false
         } else {
             return false
         }
     }
     
     func getUserData() -> User? {
-        get(forKey: key)
+        UserStorageManager.currentUser
     }
     
-    private func set(object: User, forKey key: String) {
+    private func set(object: [User], forKey key: String) {
         let jsonData = try? JSONEncoder().encode(object)
         userDefaults.set(jsonData, forKey: key)
     }
     
-    private func get(forKey key: String) -> User? {
+    private func get(forKey key: String) -> [User]? {
         guard let data = userDefaults.object(forKey: key) as? Data else { return nil }
-        return try? JSONDecoder().decode(User?.self, from: data)
+        return try? JSONDecoder().decode([User]?.self, from: data)
     }
     
     
